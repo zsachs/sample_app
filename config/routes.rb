34cadf66496a59d025cba0224e7043fa614f949c
Zachariah Sachs <zsachs@users.noreply.github.com>
2016-04-27 11:16:20 -0500

    Add Contact page; replace stubs with named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
