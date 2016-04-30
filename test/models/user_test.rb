3344fa0198da86848f016e55ae8e1857d0f931cf
Zachariah Sachs <zsachs@users.noreply.github.com>
2016-04-29 20:28:35 -0500

    Validate length of User attributes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,7 +17,16 @@ class UserTest < ActiveSupport::TestCase
 
   test "email should be present" do
     @user.email = "       "
-    assert_not @user.name.blank?
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.name = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 end
