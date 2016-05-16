1d69e23e829fd5928db0e7f5231d4fee371beb30
Zachariah Sachs <zsachs@users.noreply.github.com>
2016-05-16 13:57:32 -0500

    Test error messages for invalid user signup (solves exercise 7.2)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 								password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation div', "The form contains 4 errors."
+    assert_select 'div#error_explanation' do |elements|
+      elements.each do |element|
+        assert_select element, 'li', 4
+        assert_select element, 'li', "Name can't be blank"
+        assert_select element, 'li', "Email is invalid"
+        assert_select element, 'li',
+				"Password confirmation doesn't match Password"
+        assert_select element, 'li',
+				"Password is too short (minimum is 6 characters)"
+      end
+    end
+    assert_select 'div.field_with_errors label', 4
+    assert_select 'div.field_with_errors input', 4
+    assert_select 'div.field_with_errors input#user_name'
+    assert_select 'div.field_with_errors input#user_email'
+    assert_select 'div.field_with_errors input#user_password'
+    assert_select 'div.field_with_errors input#user_password_confirmation'
   end
 
   test "valid signup information" do
