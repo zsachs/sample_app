81fada48395ed386a2f24af3e60b7a52c49d43a2
Zachariah Sachs <zsachs@users.noreply.github.com>
2016-05-25 13:54:22 -0500

    Add and pass successful user edit acceptance test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a succesful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
