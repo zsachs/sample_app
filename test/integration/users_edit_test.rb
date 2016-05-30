4045c30bd4fec698fc74c55356855a40ab838143
Zachariah Sachs <zsachs@users.noreply.github.com>
2016-05-29 21:24:03 -0500

    Test friendly forwarding reverts to its default (solves exercise 9.1)

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,8 +18,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_equal "http://www.example.com"+edit_user_path(@user), session[:forwarding_url]
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name: name,
