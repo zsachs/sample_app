3261e0b61a59f227463187b4fc378c497947073c
Zachariah Sachs <zsachs@users.noreply.github.com>
2016-06-01 22:13:46 -0500

    Add a PasswordResets resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,17 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
-
-  get 'help' => 'static_pages#help'
-
-  get 'about' => 'static_pages#about'
-
-  get 'contact' => 'static_pages#contact'
-
-  get 'signup' => 'users#new'
-
-  get 'login' => 'sessions#new'
-
-  post 'login' => 'sessions#create'
-
-  delete 'logout' => 'sessions#destroy'
-
+  root					'static_pages#home'
+  get	'help'		=>	'static_pages#help'
+  get	'about'		=>	'static_pages#about'
+  get	'contact'	=>	'static_pages#contact'
+  get	'signup'	=>	'users#new'
+  get	'login'		=>	'sessions#new'
+  post	'login'		=>	'sessions#create'
+  delete 'logout'	=>	'sessions#destroy'
   resources :users
-
-  resources :account_activations, only: [:edit]
+  resources :account_activations, 	only: [:edit]
+  resources :password_resets, 		only: [:new, :create, :edit, :update]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
